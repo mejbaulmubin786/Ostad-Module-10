224d38a81d668eaca9b52d73046ebd176d23a4f5
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2024-08-19 21:10:05 +0600

    Update string.php

--- string.php
+++ string.php
@@ -1,1 +1,15 @@
+<?php
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach ($strings as $string) {
+    // Count vowels
+    $vowelCount = preg_match_all('/[aeiou]/i', $string);
+
+    // Reverse the string
+    $reversedString = strrev($string);
+
+    // Print the result
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
+?>
 
